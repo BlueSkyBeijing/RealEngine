4440278b577b8573883df6ab7ee5bf54fb0c6f4c
Junlong Chen <chenjunlong@kingsoft.com>
2020-01-22 15:41:57 +0800

    Engine frame codes

--- Engine/Frame/Public/Device.h
+++ Engine/Frame/Public/Device.h
@@ -5,8 +5,11 @@ class IDevice
 public:
 	virtual int Init() = 0;
 	virtual int UnInit() = 0;
-	virtual int Clear() = 0;
-	virtual int Draw() = 0;
+	virtual int Clear() = 0; 
+	virtual int SetTarget() = 0;
+	virtual int SetPipelineState() = 0;
+	virtual int Draw() = 0; 
+	virtual int Present() = 0;
 };
 
 class Device : public IDevice
@@ -15,6 +18,10 @@ public:
 	virtual int Init() override;
 	virtual int UnInit() override;
 	virtual int Clear() override;
-	virtual int Draw() override;
+	virtual int SetTarget() override;
+	virtual int SetPipelineState() override;
+	virtual int Draw() override; 
+	virtual int Present() override;
+
 };
 
