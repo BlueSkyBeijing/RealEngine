4eafaf0b31aa9d1a50b2c4415eefcc2b26df6946
Junlong Chen <chenjunlong@kingsoft.com>
2020-01-23 11:54:37 +0800

    Engine frame codes

--- Engine/Frame/Private/Device.cpp
+++ Engine/Frame/Private/Device.cpp
@@ -19,3 +19,20 @@ int Device::Clear()
 {
 	return 0;
 }
+
+int Device::SetTarget()
+{
+	return 0;
+}
+
+int Device:: SetPipelineState()
+{
+	return 0;
+}
+
+int Device::Present()
+{
+	return 0;
+}
+
+
