435fcd0f8511231e3cc16b506977d2e49c81def8
Junlong Chen <chenjunlong@kingsoft.com>
2020-01-24 19:12:21 +0800

    Engine frame codes

--- Engine/Frame/Private/Texture.cpp
+++ Engine/Frame/Private/Texture.cpp
@@ -1,2 +1,21 @@
 #include "..\Public\Texture.h"
 
+int Texture::Load()
+{
+  return 0;
+}
+
+const int GetWidth()
+{
+  return Width;
+}
+
+const int GetHeight()
+{
+  return Height;
+}
+
+const int GetFormat()
+{
+  return Format;
+}
