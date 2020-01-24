127b10d6e1ab0e6780114d62e24c05986a88dc6e
Junlong Chen <chenjunlong@kingsoft.com>
2020-01-24 19:09:24 +0800

    Engine frame codes

--- Engine/Frame/Public/Texture.h
+++ Engine/Frame/Public/Texture.h
@@ -4,5 +4,20 @@ class ITexture
 {
 public:
 	virtual int Load() = 0;
+	virtual const int GetWidth() = 0;
+	virtual const int GetHeight() = 0;
+	virtual const int GetFormat() = 0;
 };
 
+class Texture : public ITexture
+{
+public:
+	virtual int Load() override;
+	virtual const int GetWidth() override;
+	virtual const int GetHeight() override;
+	virtual const int GetFormat() override;
+protected:
+int Width;
+int Hight;
+int Format;
+};
