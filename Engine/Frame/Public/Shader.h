7460d6f1961d320b3f79f29eaae067db98a51df5
Junlong Chen <chenjunlong@kingsoft.com>
2020-01-25 16:18:44 +0800

    Engine frame codes

--- Engine/Frame/Public/Shader.h
+++ Engine/Frame/Public/Shader.h
@@ -1,14 +1,29 @@
 #pragma once
 
+enum ShaderType
+{
+VertexShader,
+PixelShader,
+GeometryShader,
+HullShader,
+DomainShader,
+}
+
 class IShader
 {
 public:
 	virtual int Load() = 0;
+	virtual int Combline() = 0;
+	virtual const int GetName() = 0;
+	virtual const ShaderType GetType() = 0;
 };
 
 class Shader : public IShader
 {
 public:
-	virtual int Load() override;
+	virtual int Load() override;	
+	virtual int Combline() override;
+	virtual const int GetName() override;
+	virtual const ShaderType GetType() override;
 };
 
