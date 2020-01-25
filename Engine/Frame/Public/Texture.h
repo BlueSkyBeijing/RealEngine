913a580ed4c71afe5818f0868f8f8cb5f1181210
Junlong Chen <chenjunlong@kingsoft.com>
2020-01-25 16:36:31 +0800

    Engine frame codes

--- Engine/Frame/Public/Texture.h
+++ Engine/Frame/Public/Texture.h
@@ -1,23 +1,36 @@
 #pragma once
 
+enum PixelFormat
+{
+	R8G8B8,
+}
+
+enum TextureFileFormat
+{
+	TGA,
+}
+
 class ITexture
 {
 public:
 	virtual int Load() = 0;
-	virtual const int GetWidth() = 0;
-	virtual const int GetHeight() = 0;
-	virtual const int GetFormat() = 0;
+	virtual const int GetWidth() const = 0;
+	virtual const int GetHeight() const = 0;
+	virtual const PixelFormat GetPixelFormat() const = 0;
+	virtual const TextureFileFormat GetTextureFileFormat() const = 0;
 };
 
 class Texture : public ITexture
 {
 public:
 	virtual int Load() override;
-	virtual const int GetWidth() override;
-	virtual const int GetHeight() override;
-	virtual const int GetFormat() override;
+	virtual const int GetWidth() const override;
+	virtual const int GetHeight() const override;
+	virtual const PixelFormat GetPixelFormat() const override;
+	virtual const TextureFileFormat GetTextureFileFormat() const = 0;
 protected:
-int Width;
-int Hight;
-int Format;
+	int mWidth;
+	int mHight;
+	PixelFormat mPixelFormat;
+	TextureFileFormat mTextureFileFormat;
 };
