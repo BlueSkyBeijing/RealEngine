23427bf41a087ed29389f67e482b063e76a9543d
Junlong Chen <chenjunlong@kingsoft.com>
2020-01-25 16:25:32 +0800

    Engine frame codes

--- Engine/Frame/Private/Shader.cpp
+++ Engine/Frame/Private/Shader.cpp
@@ -4,3 +4,18 @@ int Shader::Load()
 {
 	return 0;
 }
+
+int Shader::Compile()
+{
+	return 0;
+}
+
+const std::string& Shader::GetName() const
+{
+	return Name;
+}
+
+const Shader GetType() const
+{
+	return Type;
+}
