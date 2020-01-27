7418b93a444a84e8bf0ba7ccd5160207dd9ab6e4
Junlong Chen <chenjunlong@kingsoft.com>
2020-01-27 17:26:22 +0800

    Engine frame codes

--- Engine/Shaders/ForwardShadingPS.hlsl
+++ Engine/Shaders/ForwardShadingPS.hlsl
@@ -5,7 +5,20 @@ SamplerState DiffuseSamplerState : register(s0);
 
 float4 PSMain(VertexOut PIn) : SV_Target
 {
+    //Base color
     float4 diffuse = DiffuseMap.Sample(DiffuseSamplerState, PIn.TexCoord);
+
+    //Shadow
+    
+    //Diffuse BRDF
+    
+    //Specular BRDF
+    
+    //Emissive
+    
+    //Sky light
+    
+    //Fog
 
     return diffuse;
 }
