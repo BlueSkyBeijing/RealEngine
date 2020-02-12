3d115ae978421080990f9945fa8cc47602977011
BlueSkyBeijing <chenjunlong@kingsoft.com>
2020-02-12 08:26:58 +0800

    add dx12 utility functions

--- Engine/RenderDevices/DX12/Public/DX12Utility.h
+++ Engine/RenderDevices/DX12/Public/DX12Utility.h
@@ -14,3 +14,17 @@ inline UINT CalcConstantBufferByteSize(UINT byteSize)
 {
 	return (byteSize + 255) & ~255;
 }
+
+static Microsoft::WRL::ComPtr<ID3DBlob> CompileShader(
+	const std::wstring& filename,
+	const D3D_SHADER_MACRO* defines,
+	const std::string& entrypoint,
+	const std::string& target);
+
+
+static Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer(
+	ID3D12Device* device,
+	ID3D12GraphicsCommandList* cmdList,
+	const void* initData,
+	UINT64 byteSize,
+	Microsoft::WRL::ComPtr<ID3D12Resource>& uploadBuffer);
